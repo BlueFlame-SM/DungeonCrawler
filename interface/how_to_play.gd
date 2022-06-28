6970647726a10110fc2b72b83cc97c1443d5ff3e
Kelly <76435051+kelly-de-dood@users.noreply.github.com>
2022-06-28 18:55:18 +0200

    Changed buttons How To Play and Pause + comments in interface script files + fixed bad sense of direction

--- interface/how_to_play.gd
+++ interface/how_to_play.gd
@@ -1,34 +1,35 @@
 extends Popup
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	""" Upon opening the How To Play, show the Gameplay (Story 
+		explenation). """
 	$Background/Keybindings.visible = false
 	$Background/Gameplay.visible = true
 
+
 func _unhandled_input(event):
+	""" Close the How To Play menu upon pressing Esc. """
 	if event.is_action_pressed("ui_cancel"):
 		hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Close_pressed():
+	""" Close the How To Play menu upon pressing the Close button. """
 	hide()
 
 
 func _on_Keybindings_pressed():
+	""" Upon selecting Keybindings, show the Keybindings node and hide the 
+		Gameplay node.
+	"""
 	$Background/Keybindings.visible = true
 	$Background/Gameplay.visible = false
 
 
 func _on_Gameplay_pressed():
+	""" Upon selecting Gameplay, show the Gameplay node and hide the 
+		Keybindings node.
+	"""
 	$Background/Gameplay.visible = true
 	$Background/Keybindings.visible = false
