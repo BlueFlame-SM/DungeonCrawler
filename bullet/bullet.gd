f15a5ce40c9717b3cb221ab5b36e3425e20936a3
Timo Post <timo.post@student.uva.nl>
2022-06-28 15:07:11 +0200

    Mode bullet initializable

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -1,26 +1,34 @@
-extends Area2D
 class_name Bullet
+extends Area2D
+
+var velocity = Vector2(0, 200)
+var damage = 1
 
 
-var move = Vector2.ZERO
-var look_vec = Vector2.ZERO
-var speed = 3
+func init(pos:Vector2=position, vel:Vector2=velocity, dmg:int=damage):
+	"""
+	Initializes a bullet.
 
-
-func _ready():
-	look_vec = Player.position - global_position
+	Parameters
+	----------
+	pos: Vector2, default=Vector2.ZERO
+		The initial position of the bullet.
+	vel: Vector2, default=Vector2(0,200)
+		The velocity of the bullet.
+	dmg: int, default = 1
+		The amount of damage dealt on hit.
+	"""
+	position = pos
+	velocity = vel
+	damage = max(0, dmg)
+	return self
 
 
 func _physics_process(delta):
-	move = Vector2.ZERO
-
-	move = move.move_toward(look_vec, delta)
-	move = move.normalized() * speed
-	position += move
+	position += velocity * delta
 
 
 func _on_Bullet_body_entered(body):
 	if body == Player:
-		#TODO: hardcode weghalen
-		Player.health -= 2
+		Player.health -= damage
 	queue_free()
